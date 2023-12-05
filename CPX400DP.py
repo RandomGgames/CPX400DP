71eb85b95d907ada7db3220f87eb4a306286bfc3
RandomGgames <gianopp@gmail.com>
2023-12-05 16:49:58 -0500

    Update CPX400DP.py

--- CPX400DP.py
+++ CPX400DP.py
@@ -8,34 +8,36 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import typing
 
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 
 class CPX400DP:
     documentation_url = 'https://resources.aimtti.com/manuals/CPX400DP_Instruction_Manual-Iss1.pdf#page=28'
     
     def __init__(self) -> None:
+        self.com_port = None
         self.serialConnection = None
     
     def __str__(self):
         return str(vars(self))
     
-    def connect(self, port: str, baudrate: int = 9600) -> None:
+    def connect(self, com_port: str, baudrate: int = 9600) -> None:
         """
-        Connect to a serial port with the specified baud rate.
+        Connect to a serial com_port with the specified baud rate.
         
         Parameters:
-        - port (str): The name or identifier of the serial port.
+        - com_port (str): The name or identifier of the serial com_port.
         - baudrate (int, optional): The baud rate for the serial connection. Default is 9600.
         
         Raises:
-        - Exception: If an error occurs while connecting to the specified port.
+        - Exception: If an error occurs while connecting to the specified com_port.
         """
         try:
-            logger.debug(f'Connecting to "{port}" with speed "{baudrate}"...')
-            self.serialConnection = serial.Serial(port, baudrate)
-            logger.debug(f'Connected to "{port}" with speed "{baudrate}".')
+            logger.debug(f'CPX400DP: Connecting to "{com_port}" with speed "{baudrate}"...')
+            self.serialConnection = serial.Serial(com_port, baudrate)
+            self.com_port = com_port
+            logger.debug(f'CPX400DP: Connected to "{com_port}" with speed "{baudrate}".')
         except Exception as e:
-            logger.exception(f'An error occurred while connecting to port due to {repr(e)}.')
+            logger.exception(f'CPX400DP: An error occurred while connecting to com_port due to {repr(e)}.')
             raise e
     
     def autoConnect(self) -> None:
@@ -43,11 +45,11 @@ class CPX400DP:
         Automatically connect to a CPX400DP device if 1 is found.
         
         Raises:
-        - ValueError: If no CPX400DP supplies are detected or if multiple supplies are detected without specifying a specific com port.
+        - ValueError: If no CPX400DP supplies are detected or if multiple supplies are detected without specifying a specific com com_port.
         - Exception: If an error occurs during the automatic connection process.
         """
         try:
-            logger.debug('Automatically connecting to device...')
+            logger.debug('CPX400DP: Automatically connecting to device...')
             comports = list(serial.tools.list_ports.comports())
             power_supply_comports = [comport for comport in comports if comport.hwid[:21] == 'USB VID:PID=103E:0460']
             
@@ -57,30 +59,34 @@ class CPX400DP:
                     raise ConnectionError('No CPX400DP supplies detected.')
                 case 1:
                     power_supply_comport = power_supply_comports[0]
-                    port = power_supply_comport.device
-                    self.connect(port)
+                    com_port = power_supply_comport.device
+                    self.connect(com_port)
+                    self.com_port = com_port
                 case _:
                     raise ConnectionError('Multiple CPX400DP supplies detected. One must be specified')
         except ValueError as e:
             logger.error(str(e))
             raise e
         except Exception as e:
-            logger.error(f'An error occurred while automatically connecting to device due to {repr(e)}')
+            if self.com_port is None:
+                logger.error(f'CPX400DP: An error occurred while automatically connecting to device due to {repr(e)}')
+            else:
+                logger.error(f'CPX400DP {self.com_port}: An error occurred while automatically connecting to device due to {repr(e)}')
             raise e
     
     def disconnect(self) -> None:
         """
-        Disconnect from the currently connected serial port.
+        Disconnect from the currently connected serial com_port.
         
         Raises:
         - Exception: If an error occurs while terminating the connection.
         """
         try:
-            logger.debug(f'Terminating connection...')
+            logger.debug(f'CPX400DP {self.com_port}: Terminating connection...')
             self.serialConnection.close()
-            logger.debug(f'Terminated connection.')
+            logger.debug(f'CPX400DP {self.com_port}: Terminated connection.')
         except Exception as e:
-            logger.exception(f'An error occured while terminating connection due to {repr(e)}.')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while terminating connection due to {repr(e)}.')
             raise e
     
     def setVoltage(self, voltage: float, channel: int = 1) -> None:
@@ -95,11 +101,11 @@ class CPX400DP:
         - Exception: If an error occurs while setting the voltage.
         """
         try:
-            logger.debug(f'Setting voltage for channel {channel} to {voltage}V...')
+            logger.debug(f'CPX400DP {self.com_port}: Setting voltage for channel {channel} to {voltage}V...')
             self.serialConnection.write(bytes(f'V{channel} {voltage}\n'.encode('utf-8')))
-            logger.debug(f'Set voltage for channel {channel} to {voltage}V.')
+            logger.debug(f'CPX400DP {self.com_port}: Set voltage for channel {channel} to {voltage}V.')
         except Exception as e:
-            logger.exception(f'An error occured while setting voltage due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while setting voltage due to {repr(e)}')
             raise e
     
     def setCurrent(self, current: float, channel: int = 1) -> None:
@@ -114,11 +120,11 @@ class CPX400DP:
         - Exception: If an error occurs while setting the current.
         """
         try:
-            logger.debug(f'Setting current for channel {channel} to {current}A...')
+            logger.debug(f'CPX400DP {self.com_port}: Setting current for channel {channel} to {current}A...')
             self.serialConnection.write(bytes(f'I{channel} {current}\n'.encode('utf-8')))
-            logger.debug(f'Set current for channel {channel} to {current}A.')
+            logger.debug(f'CPX400DP {self.com_port}: Set current for channel {channel} to {current}A.')
         except Exception as e:
-            logger.exception(f'An error occured while setting current due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while setting current due to {repr(e)}')
             raise e
     
     def enableOutput(self, channel: int = 1) -> None:
@@ -132,11 +138,11 @@ class CPX400DP:
         - Exception: If an error occurs while enabling the channel.
         """
         try:
-            logger.debug(f'Enabling channel {channel}...')
+            logger.debug(f'CPX400DP {self.com_port}: Enabling channel {channel}...')
             self.serialConnection.write(bytes(f'OP{channel} 1\n'.encode('utf-8')))
-            logger.debug(f'Enabled channel {channel}.')
+            logger.debug(f'CPX400DP {self.com_port}: Enabled channel {channel}.')
         except Exception as e:
-            logger.exception(f'An error occured while enabling channel {channel} due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while enabling channel {channel} due to {repr(e)}')
             raise e
     
     def disableOutput(self, channel: int = 1) -> None:
@@ -150,11 +156,11 @@ class CPX400DP:
         - Exception: If an error occurs while enabling the channel.
         """
         try:
-            logger.debug(f'Disabling channel {channel}...')
+            logger.debug(f'CPX400DP {self.com_port}: Disabling channel {channel}...')
             self.serialConnection.write(bytes(f'OP{channel} 0\n'.encode('utf-8')))
-            logger.debug(f'Disabled channel {channel}.')
+            logger.debug(f'CPX400DP {self.com_port}: Disabled channel {channel}.')
         except Exception as e:
-            logger.exception(f'An error occured while disabling channel {channel} due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while disabling channel {channel} due to {repr(e)}')
             raise e
     
     def getSetVoltage(self, channel: int = 1) -> float:
@@ -171,12 +177,12 @@ class CPX400DP:
         - Exception: If an error occurs while getting the set voltage.
         """
         try:
-            logger.debug(f'Getting set voltage for channel {channel}...')
+            logger.debug(f'CPX400DP {self.com_port}: Getting set voltage for channel {channel}...')
             self.serialConnection.write(bytes(f'V{channel}?\n'.encode('utf-8')))
-            logger.debug(f'Got set voltage for channel {channel}.')
+            logger.debug(f'CPX400DP {self.com_port}: Got set voltage for channel {channel}.')
             return float(self.serialConnection.readline()[3:].decode('utf-8').strip())
         except Exception as e:
-            logger.exception(f'An error occured while getting set voltage due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while getting set voltage due to {repr(e)}')
             raise e
     
     def getSetCurrent(self, channel: int = 1) -> float:
@@ -193,12 +199,12 @@ class CPX400DP:
         - Exception: If an error occurs while getting the set current.
         """
         try:
-            logger.debug(f'Getting set current for channel {channel}...')
+            logger.debug(f'CPX400DP {self.com_port}: Getting set current for channel {channel}...')
             self.serialConnection.write(bytes(f'I{channel}?\n'.encode('utf-8')))
-            logger.debug(f'Got set current for channel {channel}.')
+            logger.debug(f'CPX400DP {self.com_port}: Got set current for channel {channel}.')
             return float(self.serialConnection.readline()[3:].decode('utf-8').strip())
         except Exception as e:
-            logger.exception(f'An error occured while getting set current due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while getting set current due to {repr(e)}')
             raise e
     
     def getOutputVoltage(self, channel: int = 1) -> float:
@@ -215,13 +221,13 @@ class CPX400DP:
         - Exception: If an error occurs while getting the output voltage.
         """
         try:
-            logger.debug(f'Getting output voltage for channel {channel}...')
+            logger.debug(f'CPX400DP {self.com_port}: Getting output voltage for channel {channel}...')
             self.serialConnection.write(bytes(f'V{channel}O?\n'.encode('utf-8')))
             voltage = float(''.join(filter(lambda char: char.isdigit() or char == '.', self.serialConnection.readline().decode('utf-8'))))
-            logger.debug(f'Got output voltage for channel {channel}.')
+            logger.debug(f'CPX400DP {self.com_port}: Got output voltage for channel {channel}.')
             return voltage
         except Exception as e:
-            logger.exception(f'An error occured while getting output voltage due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while getting output voltage due to {repr(e)}')
             raise e
     
     def getOutputCurrent(self, channel: int = 1) -> float:
@@ -238,13 +244,13 @@ class CPX400DP:
         - Exception: If an error occurs while getting the output voltage.
         """
         try:
-            logger.debug(f'Getting output current for channel {channel}...')
+            logger.debug(f'CPX400DP {self.com_port}: Getting output current for channel {channel}...')
             self.serialConnection.write(bytes(f'I{channel}O?\n'.encode('utf-8')))
             current = float(''.join(filter(lambda char: char.isdigit() or char == '.', self.serialConnection.readline().decode('utf-8'))))
-            logger.debug(f'Got output current for channel {channel}.')
+            logger.debug(f'CPX400DP {self.com_port}: Got output current for channel {channel}.')
             return current
         except Exception as e:
-            logger.exception(f'An error occured while getting output current due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while getting output current due to {repr(e)}')
             raise e
     
     def getOutputStatus(self, channel: int = 1) -> typing.Literal["ON", "OFF"]:
@@ -263,13 +269,13 @@ class CPX400DP:
         - Exception: If an error occurs while getting the output status.
         """
         try:
-            logger.debug(f'Getting output status for channel {channel}...')
+            logger.debug(f'CPX400DP {self.com_port}: Getting output status for channel {channel}...')
             self.serialConnection.write(bytes(f'OP{channel}?\n'.encode('utf-8')))
             status = "ON" if int(self.serialConnection.readline().decode('utf-8')[0]) == 1 else "OFF"
-            logger.debug(f'Got output status for channel {channel}.')
+            logger.debug(f'CPX400DP {self.com_port}: Got output status for channel {channel}.')
             return status
         except Exception as e:
-            logger.exception(f'An error occured while getting output current due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while getting output current due to {repr(e)}')
             raise e
     
     def lock(self) -> None:
@@ -280,11 +286,11 @@ class CPX400DP:
         - Exception: If an error occurs while locking the settings.
         """
         try:
-            logger.debug(f'Locking settings...')
+            logger.debug(f'CPX400DP {self.com_port}: Locking settings...')
             self.serialConnection.write(bytes('IFLOCK\n'.encode('utf-8')))
-            logger.debug(f'Locked settings.')
+            logger.debug(f'CPX400DP {self.com_port}: Locked settings.')
         except Exception as e:
-            logger.exception(f'An error occured while locking settings due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while locking settings due to {repr(e)}')
     
     def unlock(self) -> None:
         """
@@ -294,11 +300,11 @@ class CPX400DP:
         - Exception: If an error occurs while locking the settings.
         """
         try:
-            logger.debug(f'Unlocking settings...')
+            logger.debug(f'CPX400DP {self.com_port}: Unlocking settings...')
             self.serialConnection.write(bytes('IFUNLOCK\n'.encode('utf-8')))
-            logger.debug(f'Unlocked settings.')
+            logger.debug(f'CPX400DP {self.com_port}: Unlocked settings.')
         except Exception as e:
-            logger.exception(f'An error occured while unlocking settings due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while unlocking settings due to {repr(e)}')
     
     def getIdentification(self) -> typing.Dict[str, str]:
         """
@@ -311,14 +317,14 @@ class CPX400DP:
         - Exception: If an error occurs while getting the identification information.
         """
         try:
-            logger.debug(f'Getting identification info...')
+            logger.debug(f'CPX400DP {self.com_port}: Getting identification info...')
             self.serialConnection.write(bytes(f'*IDN?\n'.encode('utf-8')))
             data = self.serialConnection.readline().decode('utf-8').split(', ')
             data_dict = {'name': data[0], 'model': data[1], 'sn': data[2], 'version': data[3][:-2]}
-            logger.debug(f'Got identification info.')
+            logger.debug(f'CPX400DP {self.com_port}: Got identification info.')
             return data_dict
         except Exception as e:
-            logger.exception(f'An error occured while getting output current due to {repr(e)}')
+            logger.exception(f'CPX400DP {self.com_port}: An error occured while getting output current due to {repr(e)}')
             raise e
 
 class GuiApp:
